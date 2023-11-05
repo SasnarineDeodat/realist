3fe50a41cdced32f59b09c1e17b54566dbfb11a9
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-05 14:39:23 -0500

    How to send email using AWS SES?

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,5 @@
+import * as config from "../config.js";
+
 export const welcome = (req, res) => {
   res.json({
     data: "hello from nodejs api from routes yay",
@@ -6,21 +8,43 @@ export const welcome = (req, res) => {
 
 export const preRegister = async (req, res) => {
   // create jwt with email and password then email as clickable link
-  // only when user click on that email link, registration complete
+  // only when user click on that email link, registeration completes
   try {
     console.log(req.body);
 
-    const emailSent = true;
-
-    if (emailSent) {
-      return res.json({ ok: true });
-    } else {
-      return res.json({ ok: false });
-    }
+    config.AWSSES.sendEmail(
+      {
+        Source: config.EMAIL_FROM,
+        Destination: {
+          ToAddresses: ["sasnarinedeodat407@gmail.com"],
+        },
+        Message: {
+          Body: {
+            Html: {
+              Charset: "UTF-8",
+              Data: `
+                <h1>Welcome to Realist App</h1>
+              `,
+            },
+          },
+          Subject: {
+            Charset: "UTF-8",
+            Data: "Welcome to Realist",
+          },
+        },
+      },
+      (err, data) => {
+        if (err) {
+          console.log(err);
+          return res.json({ ok: false });
+        } else {
+          console.log(data);
+          return res.json({ ok: true });
+        }
+      },
+    );
   } catch (err) {
     console.log(err);
-    res.json({
-      error: "Something went wrong. Try again.",
-    });
+    return res.json({ error: "Something went wrong. Try again." });
   }
 };
