ca565b03bc5c4f4010d20bccfd9c9c8977582b30
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-07 16:52:31 -0500

    How to implement user password update?

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -231,3 +231,33 @@ export const publicProfile = async (req, res) => {
     });
   }
 };
+
+export const updatePassword = async (req, res) => {
+  try {
+    const { password } = req.body;
+
+    if (!password) {
+      return res.json({
+        error: "Password is required",
+      });
+    }
+    if (password && password?.length < 6) {
+      return res.json({
+        error: "Password should be min 6 characters",
+      });
+    }
+
+    const user = await User.findByIdAndUpdate(req.user._id, {
+      password: await hashPassword(password),
+    });
+
+    res.json({
+      ok: true,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(403).json({
+      error: "Unauthorized",
+    });
+  }
+};
