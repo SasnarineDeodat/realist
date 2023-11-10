e7a3ca3e754f133b74e32baae74549b869c91066
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-10 14:18:17 -0500

    Register the user by sending JWT

--- client/src/pages/auth/AccountActivate.js
+++ client/src/pages/auth/AccountActivate.js
@@ -1,9 +1,35 @@
-import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useAuth } from "../../context/auth";
 
 export default function AccountActivate() {
+  //context
+  const [auth, setAuth] = useAuth();
   // hooks
   const { token } = useParams();
-  console.log(token);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token) requestActivation();
+  }, [token]);
+
+  const requestActivation = async () => {
+    try {
+      const { data } = await axios.post(`/register`, { token });
+      if (data?.error) {
+        toast.error(data.error);
+      } else {
+        setAuth(data);
+        toast.success("Successfully logged in. Welcome to Realist app.");
+        navigate("/");
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error("Something went wrong. Try again");
+    }
+  };
   return (
     <div
       className="display-1 d-flex justify-content-center align-items-center vh-100"
