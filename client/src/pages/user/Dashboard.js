cdfc2069185bb0c624326ca61c2da6c5ba9ab67e
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-11 16:45:57 -0500

    How to protect pages from not logged-in user?

--- client/src/pages/user/Dashboard.js
+++ client/src/pages/user/Dashboard.js
@@ -1,8 +1,6 @@
 import Sidebar from "../../components/nav/Sidebar";
-import { useAuth } from "../../context/auth";
 
-export default function Home() {
-  const [auth, setAuth] = useAuth();
+export default function Dashboard() {
   return (
     <div>
       <h1 className="display-1 bg-primary text-light p-5">Dashboard</h1>
