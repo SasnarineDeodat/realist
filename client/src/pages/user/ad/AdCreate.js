38f8fa9b5314b2df7ec3b21ddbd684832f8fc233
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-12 15:41:42 -0500

    Create Ad page with options to sell or rent

--- client/src/pages/user/ad/AdCreate.js
+++ client/src/pages/user/ad/AdCreate.js
@@ -1,10 +1,49 @@
+import { useState } from "react";
 import Sidebar from "../../../components/nav/Sidebar";
+import { useNavigate } from "react-router-dom";
 
 export default function AdCreate() {
+  const [sell, setSell] = useState(false);
+  const [rent, setRent] = useState(false);
+  //hooks
+  const navigate = useNavigate();
+
+  const handleSell = () => {
+    setSell(true);
+    setRent(false);
+  };
+  const handleRent = () => {
+    setRent(true);
+    setSell(false);
+  };
   return (
     <div>
       <h1 className="display-1 bg-primary text-light p-5">Ad Create</h1>
       <Sidebar />
+      <div
+        className="d-flex justify-content-center align-items-center vh-100"
+        style={{ marginTop: "-14%" }}
+      >
+        <div className="col-lg-6">
+          <button
+            onClick={handleSell}
+            className="btn btn-primary btn-lg col-12 p-5"
+          >
+            Sell
+          </button>
+          {sell && "show sell house or land option"}
+        </div>
+
+        <div className="col-lg-6">
+          <button
+            onClick={handleRent}
+            className="btn btn-primary btn-lg col-12 p-5"
+          >
+            Rent
+          </button>
+          {rent && "show rent house or land option"}
+        </div>
+      </div>
     </div>
   );
 }
