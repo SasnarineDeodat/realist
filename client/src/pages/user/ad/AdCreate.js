bc5de71f49e3c311fa337907685f3317ac0e3bad
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-12 16:23:46 -0500

    How to use single Ad create form for selling or renting house and land?

--- client/src/pages/user/ad/AdCreate.js
+++ client/src/pages/user/ad/AdCreate.js
@@ -22,7 +22,7 @@ export default function AdCreate() {
       <Sidebar />
       <div
         className="d-flex justify-content-center align-items-center vh-100"
-        style={{ marginTop: "-9%" }}
+        style={{ marginTop: "-16%" }}
       >
         <div className="col-lg-6">
           <button
