c687b814d73cdd8fa19b3f06a7a1581450825a93
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-12 18:49:05 -0500

    How to use google places to auto-complete address dropdown?

--- client/src/components/forms/AdForm.js
+++ client/src/components/forms/AdForm.js
@@ -1,8 +1,40 @@
+import { useState } from "react";
+import GooglePlacesAutoComplete from "react-google-places-autocomplete";
+import { GOOGLE_PLACES_KEY } from "../../config";
+
 export default function AdForm({ action, type }) {
+  // state
+  const [ad, setAd] = useState({
+    photos: [],
+    uploading: false,
+    price: "",
+    address: "",
+    bedrooms: "",
+    bathrooms: "",
+    carpark: "",
+    landsize: "",
+    type: "",
+    title: "",
+    description: "",
+    loading: false,
+  });
   return (
     <>
-      <p>This is ad create form</p>
-      {action} / {type}
+      <div className="mb-3 form-control">
+        <GooglePlacesAutoComplete
+          apiKey={GOOGLE_PLACES_KEY}
+          apiOptions="us"
+          selectProps={{
+            defaultInputValue: ad?.address,
+            placeholder: "Search for address...",
+            onChange: ({ value }) => {
+              setAd({ ...ad, address: value.description });
+            },
+          }}
+        />
+      </div>
+
+      <pre>{JSON.stringify(ad, null, 4)}</pre>
     </>
   );
 }
