b2a00d72c5e7c10978e03b8dbc39c85e18f77273
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-13 17:45:35 -0500

    How to upload image to AWS S3 from NodeJs?

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import { DATABASE } from "./config.js";
 import authRoutes from "./routes/auth.js";
+import adRoutes from "./routes/ad.js";
 
 const app = express();
 
@@ -14,10 +15,11 @@ mongoose
   .catch((err) => console.log(err));
 
 //middlewares
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(morgan("dev"));
 app.use(cors());
 //routes middleware
 app.use("/api", authRoutes);
+app.use("/api", adRoutes);
 
 app.listen(8000, () => console.log("Server_running_on_port_8000"));
