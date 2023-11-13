04d941845141a5377da545ed1acb9fb54b43feb7
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-12 19:10:22 -0500

    Add additional form fields to the Ad create form

--- client/src/components/forms/AdForm.js
+++ client/src/components/forms/AdForm.js
@@ -45,6 +45,87 @@ export default function AdForm({ action, type }) {
         onValueChange={(value) => setAd({ ...ad, price: value })}
       />
 
+      <input
+        type="number"
+        min="0"
+        className="form-control mb-3"
+        placeholder="Enter how many bedrooms"
+        value={ad.bedrooms}
+        onChange={(e) =>
+          setAd({
+            ...ad,
+            bedrooms: e.target.value,
+          })
+        }
+      />
+
+      <input
+        type="number"
+        min="0"
+        className="form-control mb-3"
+        placeholder="Enter how many bathrooms"
+        value={ad.bathrooms}
+        onChange={(e) =>
+          setAd({
+            ...ad,
+            bathrooms: e.target.value,
+          })
+        }
+      />
+
+      <input
+        type="number"
+        min="0"
+        className="form-control mb-3"
+        placeholder="Enter how many carpark"
+        value={ad.carpark}
+        onChange={(e) =>
+          setAd({
+            ...ad,
+            carpark: e.target.value,
+          })
+        }
+      />
+
+      <input
+        type="text"
+        className="form-control mb-3"
+        placeholder="Size of land"
+        value={ad.landsize}
+        onChange={(e) =>
+          setAd({
+            ...ad,
+            landsize: e.target.value,
+          })
+        }
+      />
+
+      <input
+        type="text"
+        className="form-control mb-3"
+        placeholder="Enter title"
+        value={ad.title}
+        onChange={(e) =>
+          setAd({
+            ...ad,
+            title: e.target.value,
+          })
+        }
+      />
+
+      <textarea
+        className="form-control mb-3"
+        placeholder="Enter description"
+        value={ad.description}
+        onChange={(e) =>
+          setAd({
+            ...ad,
+            description: e.target.value,
+          })
+        }
+      />
+
+      <button className="btn btn-primary">Submit</button>
       <pre>{JSON.stringify(ad, null, 4)}</pre>
     </>
   );
