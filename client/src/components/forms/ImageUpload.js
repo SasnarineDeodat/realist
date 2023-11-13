0bd3dabb476183fba60520192e21269f2fc8b1cb
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-13 16:33:09 -0500

    How to resize image client side in React?

--- client/src/components/forms/ImageUpload.js
+++ client/src/components/forms/ImageUpload.js
@@ -1,11 +1,43 @@
+import Resizer from "react-image-file-resizer";
+import axios from "axios";
 export default function ImageUpload({ ad, setAd }) {
   const handleUpload = async (e) => {
     try {
       let files = e.target.files;
       files = [...files];
       if (files?.length) {
-        console.log(files);
+        // console.log(files);
         setAd({ ...ad, uploading: true });
+
+        files.map((file) => {
+          new Promise((resolve) => {
+            Resizer.imageFileResizer(
+              file,
+              1080,
+              720,
+              "JPEG",
+              100,
+              0,
+              async (uri) => {
+                try {
+                  console.log("UPLOAD URI =>", uri);
+                  const { data } = await axios.post("/upload-image", {
+                    image: uri,
+                  });
+                  setAd((prev) => ({
+                    ...prev,
+                    photos: [data, ...prev.photos],
+                    uploading: false,
+                  }));
+                } catch (err) {
+                  console.log(err);
+                  setAd({ ...ad, uploading: false });
+                }
+              },
+              "base64",
+            );
+          });
+        });
       }
 
       setAd({ ...ad, uploading: true });
