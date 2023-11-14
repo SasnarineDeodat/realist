58a7707e02e6fbfbfd6aae718ac95601c11195a4
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-14 15:52:29 -0500

    How to create Ad model with mongoose?

---
+++ server/models/ad.js
@@ -0,0 +1,62 @@
+import { model, Schema, ObjectId } from "mongoose";
+
+const schema = new Schema(
+  {
+    photos: [{}],
+    price: {
+      type: Number,
+      maxLength: 255,
+    },
+    address: {
+      type: String,
+      maxLength: 255,
+      required: true,
+    },
+    bedrooms: Number,
+    bathrooms: Number,
+    landsize: Number,
+    carpark: Number,
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        default: "Point",
+      },
+      coordinates: {
+        type: [Number],
+        default: [41.55426, -73.043068],
+      },
+    },
+    title: {
+      type: String,
+      maxLength: 255,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+      unique: true,
+    },
+    description: {},
+    postedBy: { type: ObjectId, ref: "User" },
+    sold: {
+      type: Boolean,
+      default: false,
+    },
+    googleMap: {},
+    type: {
+      type: String,
+      default: "Other",
+    },
+    action: {
+      type: String,
+      default: "Sell",
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+export default model("Ad", schema);
