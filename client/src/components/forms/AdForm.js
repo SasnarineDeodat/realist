261c873fc55ff65be7a90ff1a34921558541f4af
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-14 18:49:50 -0500

    How to show and hide form fields based on house or land?

--- client/src/components/forms/AdForm.js
+++ client/src/components/forms/AdForm.js
@@ -74,47 +74,53 @@ export default function AdForm({ action, type }) {
         />
       </div>
 
-      <input
-        type="number"
-        min="0"
-        className="form-control mb-3"
-        placeholder="Enter how many bedrooms"
-        value={ad.bedrooms}
-        onChange={(e) =>
-          setAd({
-            ...ad,
-            bedrooms: e.target.value,
-          })
-        }
-      />
+      {type === "House" ? (
+        <>
+          <input
+            type="number"
+            min="0"
+            className="form-control mb-3"
+            placeholder="Enter how many bedrooms"
+            value={ad.bedrooms}
+            onChange={(e) =>
+              setAd({
+                ...ad,
+                bedrooms: e.target.value,
+              })
+            }
+          />
 
-      <input
-        type="number"
-        min="0"
-        className="form-control mb-3"
-        placeholder="Enter how many bathrooms"
-        value={ad.bathrooms}
-        onChange={(e) =>
-          setAd({
-            ...ad,
-            bathrooms: e.target.value,
-          })
-        }
-      />
+          <input
+            type="number"
+            min="0"
+            className="form-control mb-3"
+            placeholder="Enter how many bathrooms"
+            value={ad.bathrooms}
+            onChange={(e) =>
+              setAd({
+                ...ad,
+                bathrooms: e.target.value,
+              })
+            }
+          />
 
-      <input
-        type="number"
-        min="0"
-        className="form-control mb-3"
-        placeholder="Enter how many carpark"
-        value={ad.carpark}
-        onChange={(e) =>
-          setAd({
-            ...ad,
-            carpark: e.target.value,
-          })
-        }
-      />
+          <input
+            type="number"
+            min="0"
+            className="form-control mb-3"
+            placeholder="Enter how many carpark"
+            value={ad.carpark}
+            onChange={(e) =>
+              setAd({
+                ...ad,
+                carpark: e.target.value,
+              })
+            }
+          />
+        </>
+      ) : (
+        ""
+      )}
 
       <input
         type="text"
