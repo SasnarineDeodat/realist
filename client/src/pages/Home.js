d0cca667f6cc38f0d8655a53a3a26b5ad2606183
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-14 19:42:52 -0500

    How to create re-usable card component in React?

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../context/auth";
 import axios from "axios";
+import AdCard from "../components/cards/AdCard";
 
 export default function Home() {
   // context
@@ -24,8 +25,25 @@ export default function Home() {
   };
   return (
     <div>
-      <h1 className="display-1 bg-primary text-light p-5">Home</h1>
-      <pre>{JSON.stringify({ adsForSell, adsForRent }, null, 4)}</pre>
+      <h1 className="display-1 bg-primary text-light p-5">For Sell</h1>
+
+      <div className="container">
+        <div className="row">
+          {adsForSell?.map((ad) => (
+            <AdCard ad={ad} key={ad._id} />
+          ))}
+        </div>
+      </div>
+
+      <h1 className="display-1 bg-primary text-light p-5">For Rent</h1>
+
+      <div className="container">
+        <div className="row">
+          {adsForRent?.map((ad) => (
+            <AdCard ad={ad} key={ad._id} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
