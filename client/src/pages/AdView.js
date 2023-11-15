f6ab8d8dc8d166bc17c720655073698bdecf4a32
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-15 18:34:18 -0500

    How to create image gallery in React?

--- client/src/pages/AdView.js
+++ client/src/pages/AdView.js
@@ -1,6 +1,20 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Gallery from "react-photo-gallery";
+
+const photos = [
+  {
+    src: "https://realist-bucket-123.s3.amazonaws.com/YxfcWchIbMHuB7AUIEAAC.jpeg",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://realist-bucket-123.s3.amazonaws.com/9nO1wF5L9ZPwkOHpGXO9n.jpeg",
+    width: 1,
+    height: 1,
+  },
+];
 
 export default function AdView() {
   // state
@@ -24,6 +38,7 @@ export default function AdView() {
   };
   return (
     <>
+      <Gallery photos={photos} />;
       <pre>{JSON.stringify({ ad, related }, null, 4)}</pre>
     </>
   );
