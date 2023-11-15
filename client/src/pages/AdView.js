7ca7f5c9038d070c70fcc580f5d27c01b0def1c5
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-15 18:12:58 -0500

    How to fetch individual Ad based on route params?

--- client/src/pages/AdView.js
+++ client/src/pages/AdView.js
@@ -1,6 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function AdView() {
+  // state
+  const [ad, setAd] = useState({});
+  const [related, setRelated] = useState([]);
+  // hooks
   const params = useParams();
-  return <>{params.slug}</>;
+
+  useEffect(() => {
+    if (params?.slug) fetchAd();
+  }, [params?.slug]);
+
+  const fetchAd = async () => {
+    try {
+      const { data } = await axios.get(`/ad/${params.slug}`);
+      setAd(data?.ad);
+      setRelated(data?.related);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <>
+      <pre>{JSON.stringify({ ad, related }, null, 4)}</pre>
+    </>
+  );
 }
