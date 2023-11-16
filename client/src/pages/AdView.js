89415827a4d687b265189ea53fd7f4d3ef9bf7b4
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-16 13:36:34 -0500

    How to show image gallery photos in a modal?

--- client/src/pages/AdView.js
+++ client/src/pages/AdView.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
 const photos = [
   {
@@ -20,6 +21,8 @@ export default function AdView() {
   // state
   const [ad, setAd] = useState({});
   const [related, setRelated] = useState([]);
+  const [current, setCurrent] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
   // hooks
   const params = useParams();
 
@@ -36,9 +39,33 @@ export default function AdView() {
       console.log(err);
     }
   };
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrent(index);
+    setIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrent(0);
+    setIsOpen(false);
+  };
   return (
     <>
-      <Gallery photos={photos} />;
+      <Gallery photos={photos} onClick={openLightbox} />;
+      <ModalGateway>
+        {isOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={current}
+              views={photos.map((x) => ({
+                ...x,
+                srcset: x.srcset,
+                caption: x.title,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
       <pre>{JSON.stringify({ ad, related }, null, 4)}</pre>
     </>
   );
---
+++ client/src/pages/copilot commands.md
@@ -0,0 +1,10 @@
+# copilot commands
+
+Here are the ones you should know:
+
+    Accept inline code suggestion — Tab
+    Dismiss inline code suggestion — Esc
+    Show next suggestion — Alt + ] or Option (⌥) + ]
+    Show previous suggestion — Alt + [ or Option (⌥) + [
+    Trigger suggestion — Alt + \ or Option (⌥) + \
+    Open 10 suggestions in a separate pane — Ctrl + Enter
