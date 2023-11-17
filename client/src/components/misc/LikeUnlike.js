5884e668743926b69de6349779be7c7aeb31557c
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-17 15:28:50 -0500

    How to use like and unlike feature in React?

--- client/src/components/misc/LikeUnlike.js
+++ client/src/components/misc/LikeUnlike.js
@@ -1,20 +1,46 @@
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/auth";
 import { FcLike, FcLikePlaceholder } from "react-icons/fc";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function LikeUnlike({ ad }) {
   // context
   const [auth, setAuth] = useAuth();
+  // hooks
+  const navigate = useNavigate();
 
   const handleLike = async () => {
     try {
-      //
+      if (auth.user === null) {
+        navigate("/login");
+        return;
+      }
+      const { data } = await axios.post("/wishlist", { adId: ad._id });
+      console.log("handle like =>", data);
+      setAuth({ ...auth, user: data });
+      const fromLS = JSON.parse(localStorage.getItem("auth"));
+      fromLS.user = data;
+      localStorage.setItem("auth", JSON.stringify(fromLS));
+      toast.success("Added to wishlist");
     } catch (err) {
       console.log(err);
     }
   };
+
   const handleUnlike = async () => {
     try {
-      //
+      if (auth.user === null) {
+        navigate("/login");
+        return;
+      }
+      const { data } = await axios.delete(`/wishlist/${ad._id}`);
+      console.log("handle Unlike =>", data);
+      setAuth({ ...auth, user: data });
+      const fromLS = JSON.parse(localStorage.getItem("auth"));
+      fromLS.user = data;
+      localStorage.setItem("auth", JSON.stringify(fromLS));
+      toast.success("Removed from wishlist");
     } catch (err) {
       console.log(err);
     }
