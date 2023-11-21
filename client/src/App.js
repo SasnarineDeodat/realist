096f64674f754a1ed6e73c6f9c313d9a6114594f
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-21 18:29:43 -0500

    How to create search context?

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/auth";
+import { SearchProvider } from "./context/search";
 import { Toaster } from "react-hot-toast";
 
 import Home from "./pages/Home";
@@ -32,43 +33,45 @@ function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Main />
-        <Toaster />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/auth/account-activate/:token"
-            element={<AccountActivate />}
-          />
-          <Route path="/auth/forgot-password" element={<ForgotPassword />} />
-          <Route
-            path="/auth/access-account/:token"
-            element={<AccessAccount />}
-          />
-          <Route path="/" element={<PrivateRoute />}>
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="ad/create" element={<AdCreate />} />
-            <Route path="ad/create/sell/house" element={<SellHouse />} />
-            <Route path="ad/create/sell/land" element={<SellLand />} />
-            <Route path="ad/create/rent/house" element={<RentHouse />} />
-            <Route path="ad/create/rent/land" element={<RentLand />} />
-            <Route path="user/profile" element={<Profile />} />
-            <Route path="user/settings" element={<Settings />} />
-            <Route path="user/ad/:slug" element={<AdEdit />} />
-            <Route path="user/wishlist" element={<Wishlist />} />
-            <Route path="user/enquiries" element={<Enquiries />} />
-          </Route>
+        <SearchProvider>
+          <Main />
+          <Toaster />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/auth/account-activate/:token"
+              element={<AccountActivate />}
+            />
+            <Route path="/auth/forgot-password" element={<ForgotPassword />} />
+            <Route
+              path="/auth/access-account/:token"
+              element={<AccessAccount />}
+            />
+            <Route path="/" element={<PrivateRoute />}>
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="ad/create" element={<AdCreate />} />
+              <Route path="ad/create/sell/house" element={<SellHouse />} />
+              <Route path="ad/create/sell/land" element={<SellLand />} />
+              <Route path="ad/create/rent/house" element={<RentHouse />} />
+              <Route path="ad/create/rent/land" element={<RentLand />} />
+              <Route path="user/profile" element={<Profile />} />
+              <Route path="user/settings" element={<Settings />} />
+              <Route path="user/ad/:slug" element={<AdEdit />} />
+              <Route path="user/wishlist" element={<Wishlist />} />
+              <Route path="user/enquiries" element={<Enquiries />} />
+            </Route>
 
-          <Route path="/ad/:slug" element={<AdView />} />
-          <Route path="/agents" element={<Agents />} />
-          <Route path="/agent/:username" element={<Agent />} />
+            <Route path="/ad/:slug" element={<AdView />} />
+            <Route path="/agents" element={<Agents />} />
+            <Route path="/agent/:username" element={<Agent />} />
 
-          <Route path="/buy" element={<Buy />} />
-          <Route path="/rent" element={<Rent />} />
-        </Routes>
-        <Footer />
+            <Route path="/buy" element={<Buy />} />
+            <Route path="/rent" element={<Rent />} />
+          </Routes>
+          <Footer />
+        </SearchProvider>
       </AuthProvider>
     </BrowserRouter>
   );
