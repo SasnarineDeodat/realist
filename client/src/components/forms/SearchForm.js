7acba799fc4bcb0439e14f57de5cc023c8a4fcb3
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-21 19:24:50 -0500

    How to show google places address dropdown on search form?

--- client/src/components/forms/SearchForm.js
+++ client/src/components/forms/SearchForm.js
@@ -1,8 +1,38 @@
 import { useSearch } from "../../context/search";
+import { GOOGLE_PLACES_KEY } from "../../config";
+import GooglePlacesAutoComplete from "react-google-places-autocomplete";
 
 export default function SearchForm() {
   // context
   const [search, setSearch] = useSearch();
 
-  return <>Search form</>;
+  return (
+    <>
+      <div className="container m-5">
+        <div className="row">
+          <div className="col-lg-12  form-control">
+            <GooglePlacesAutoComplete
+              apiKey={GOOGLE_PLACES_KEY}
+              apiOptions="us"
+              selectProps={{
+                defaultInputValue: search?.address,
+                placeholder: "Search for address...",
+                onChange: ({ value }) => {
+                  setSearch({ ...search, address: value.description });
+                },
+              }}
+            />
+          </div>
+        </div>
+        <div className="d-flex justify-content-center mt-3">
+          <button className="btn btn-primary col-lg-2 square">Buy</button>
+          <button className="btn btn-primary col-lg-2 square">Rent</button>
+          <button className="btn btn-primary col-lg-2 square">House</button>
+          <button className="btn btn-primary col-lg-2 square">Land</button>
+          <button className="btn btn-primary col-lg-2 square">Price</button>
+          <button className="btn btn-danger col-lg-2 square">Search</button>
+        </div>
+      </div>
+    </>
+  );
 }
