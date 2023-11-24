3b8070a8f791b140e2df480ab0dac7d04d1956d3
Robbie213 <sasnarinedeodat407@gmail.com>
2023-11-24 11:30:47 -0500

    config file removed

--- client/src/config.js
+++
@@ -1,4 +0,0 @@
-export const API = "http://localhost:8000/api";
-export const GOOGLE_PLACES_KEY = "AIzaSyCjIrrH1lsRPmD5ldiuwM5Z4fwCirQpcmQ";
-
-export const GOOGLE_MAPS_KEY = "AIzaSyBgv1tWUTnnX1pBcFB8CaZim7Equp0Jh6k";
